9ab8b5c9524ed3cc83a6a85c444781e3c2df8d36
Ryosuke <ryosuke.san.hana@gmail.com>
2020-08-24 11:01:04 -0700

    Imported theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Logo from "../components/Logo"
+import Description from "../components/Description"
+import Projects from "../components/Projects"
+import GlobalStyles from "../components/GlobalStyles"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Logo />
+      <Description />
+      <Projects />
+    </div>
+  )
 }

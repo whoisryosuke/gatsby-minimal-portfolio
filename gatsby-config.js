3f5b3d2cd104dc3ddd995689a8f0327b317a47b1
Ryosuke <ryosuke.san.hana@gmail.com>
2020-08-24 12:03:19 -0700

    SEO + Favicon + App Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,5 +49,25 @@ module.exports = {
       },
     ],
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Ryosuke",
+        short_name: "Ryosuke",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "static/favicon/android-chrome-192x192.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+  ],
 }
